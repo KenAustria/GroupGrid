6078d33442c90a9349f104c3d871230fbd2b1fad
Ken Austria <kenaustriadev@gmail.com>
2020-02-16 13:30:06 -0800

    Create a Post

--- functions/index.js
+++ functions/index.js
@@ -8,12 +8,39 @@ exports.getPosts = functions.https.onRequest((req, res) => {
 		.firestore()
 		.collection('posts')
 		.get()
-		.then((data) => {
+		.then(data => {
 			let posts = []; // declare host array
-			data.forEach((doc) => { // iterate each document of data and insert into array
+			data.forEach(doc => { // iterate each document of data and insert into array
 				posts.push(doc.data());
 			});
 			return res.json(posts);
 		})
-		.catch((err) => console.error(err));
-})
+		.catch(err => console.error(err));
+});
+
+exports.createPost = functions.https.onRequest((req, res) => {
+	// prevent client error
+	if (req.method !== 'POST') {
+		return res.status(400).json({ error: "Only POST Request Allowed."});
+	}
+
+	// newPost object's properties
+	const newPost = {
+		body: req.body.body,
+		userHandle: req.body.userHandle,
+		createdAt: admin.firestore.Timestamp.fromDate(new Date())
+	};
+
+	// add newPost object to posts collection
+	admin
+		.firestore()
+		.collection('posts')
+		.add(newPost)
+		.then(doc => {
+			return res.json({ message: "Document created successfully." });
+		})
+		.catch(err => {
+			res.status(500).json({ error: "Something went wrong."});
+			console.error(err);
+		});
+});
