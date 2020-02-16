8a86a2748b3aae578bc2e324658e82e9e09e6dbd
Ken Austria <kenaustriadev@gmail.com>
2020-02-15 21:27:39 -0800

    Get Posts

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,19 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+exports.getPosts = functions.https.onRequest((req, res) => {
+	admin // access posts collection
+		.firestore()
+		.collection('posts')
+		.get()
+		.then((data) => {
+			let posts = []; // declare host array
+			data.forEach((doc) => { // iterate each document of data and insert into array
+				posts.push(doc.data());
+			});
+			return res.json(posts);
+		})
+		.catch((err) => console.error(err));
+})
