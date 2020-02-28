84be69b40810d79ca189924940550a828cdcdf1e
Ken Austria <kenaustriadev@gmail.com>
2020-02-27 16:29:16 -0800

    React Router Setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import './App.css';
+import Home from './components/Home/Home';
+import Signup from './components/Signup/Signup';
+import Login from './components/Login/Login';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <p>Group Grid</p>
+			<Router>
+				<div className="container">
+					<Switch>
+						<Route exact path='/' component={Home}/>
+						<Route exact path='/signup' component={Signup} />
+						<Route exact path='/login' component={Login}/>
+					</Switch>
+				</div>
+      </Router>
     </div>
   );
 }
