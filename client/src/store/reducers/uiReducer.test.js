cf56f4bbf3845c3abbb51f40a6ae65cb27114a9b
Ken Austria <kenaustriadev@gmail.com>
2020-07-22 22:26:45 -0700

    Tests for uiReducer: CLEAR_ERRORS, LOADING_UI, STOP_LOADING_UI

--- client/src/store/reducers/uiReducer.test.js
+++ client/src/store/reducers/uiReducer.test.js
@@ -1,4 +1,5 @@
 import reducer from './uiReducer';
+import { SET_ERRORS, CLEAR_ERRORS, LOADING_UI, STOP_LOADING_UI } from '../actions/actionTypes';
 
 describe('uiReducer', () => {
 	it('should return the initial state', () => {
@@ -6,5 +7,49 @@ describe('uiReducer', () => {
 			loading: false,
   		errors: null
 		})
+	})
+
+	// SET_ERRORS
+
+	it('should clear errors', () => {
+		expect(reducer({
+			loading: false,
+  		errors: null
+		}, {
+			type: CLEAR_ERRORS,
+			loading: false,
+  		errors: null
+		})).toEqual({
+			loading: false,
+  		errors: null
+		});
+	})
+
+	it('should set ui', () => {
+		expect(reducer({
+			loading: false,
+  		errors: null
+		}, {
+			type: LOADING_UI,
+			loading: true,
+  		errors: null
+		})).toEqual({
+			loading: true,
+  		errors: null
+		});
+	})
+
+	it('should set ui', () => {
+		expect(reducer({
+			loading: false,
+  		errors: null
+		}, {
+			type: STOP_LOADING_UI,
+			loading: false,
+  		errors: null
+		})).toEqual({
+			loading: false,
+  		errors: null
+		});
 	})
 })
