7d84ad591eb848ba7539ba22aec5048dfec0c5b6
Ken Austria <kenaustriadev@gmail.com>
2020-08-10 19:42:32 -0700

    renderWithRedux test for MyButton

--- client/src/components/MyButton/MyButton.test.js
+++ client/src/components/MyButton/MyButton.test.js
@@ -1,40 +1,18 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import MyButton from './MyButton';
-// Test
-import { configure, shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-// Icons
-import Tooltip from '@material-ui/core/Tooltip';
-import IconButton from '@material-ui/core/IconButton';
-import AddIcon from '@material-ui/icons/Add';
-import CloseIcon from '@material-ui/icons/Close';
-import ChatIcon from '@material-ui/icons/Chat';
-import UnfoldMore from '@material-ui/icons/UnfoldMore';
-import EditIcon from '@material-ui/icons/Edit';
-import KeyboardReturn from '@material-ui/icons/KeyboardReturn';
+import { cleanup, render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import myStore from '../../store/myStore';
 
-configure({adapter: new Adapter()});
+const renderWithRedux = () => render(
+	<Provider store={myStore}>
+		<MyButton />
+	</Provider>
+);
 
-describe('<MyButton />', () => {
-	let wrapper;
+it('renders with Redux', () => {
+	const {} = renderWithRedux(<MyButton />);
+});
 
-	beforeEach(() => {
-		wrapper = shallow(<MyButton />);
-	})
-
-	it('should render a Tooltip', () => {
-		wrapper.setProps({
-			title: true,
-			tipClassName: true
-		})
-		expect(wrapper.find(Tooltip)).toHaveLength(1);
-	})
-
-	it('should render an IconButton', () => {
-		wrapper.setProps({
-			onClick: true,
-			btnClassName: true
-		})
-		expect(wrapper.find(IconButton)).toHaveLength(1);
-	})
-})
+afterEach(cleanup);
