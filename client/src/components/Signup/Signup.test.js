d45410a6c9b2ac84ba75bc13cd2916eb51671cf8
Ken Austria <kenaustriadev@gmail.com>
2020-08-17 21:48:58 -0700

    render Signup text test for Signup

--- client/src/components/Signup/Signup.test.js
+++ client/src/components/Signup/Signup.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import Signup from './Signup';
-import { cleanup, render } from '@testing-library/react';
+import { cleanup, render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import myStore from '../../store/myStore';
 
@@ -13,6 +13,11 @@ const renderWithRedux = () => render(
 
 it('renders with Redux', () => {
 	const {} = renderWithRedux(<Signup />);
+});
+
+it('renders `Signup` text', () => {
+	const {getByText} = renderWithRedux(<Signup />);
+	expect(screen.getByText(/Signup/)).toBeInTheDocument();
 });
 
 afterEach(cleanup);
