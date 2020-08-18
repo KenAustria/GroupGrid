a242216637741f227683b461a9d7cde5652970b7
Ken Austria <kenaustriadev@gmail.com>
2020-08-17 21:33:39 -0700

    render No Profile found, please login again text test for Profile

--- client/src/components/Profile/Profile.test.js
+++ client/src/components/Profile/Profile.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import Profile from './Profile';
-import { cleanup, render } from '@testing-library/react';
+import { cleanup, render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import myStore from '../../store/myStore';
 
@@ -13,6 +13,11 @@ const renderWithRedux = () => render(
 
 it('renders with Redux', () => {
 	const {} = renderWithRedux(<Profile />);
+});
+
+it('renders `No profile found, please login again` text', () => {
+	const {getByText} = renderWithRedux(<Profile />);
+	expect(screen.getByText(/No profile found, please login again/)).toBeInTheDocument();
 });
 
 afterEach(cleanup);
