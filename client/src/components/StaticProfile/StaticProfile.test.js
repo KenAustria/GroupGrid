cd536a6114eaaff99f9db2f9d80964b0a54c4748
Ken Austria <kenaustriadev@gmail.com>
2020-08-22 12:36:33 -0700

    renderWithRedux test for StaticProfile

--- client/src/components/StaticProfile/StaticProfile.test.js
+++ client/src/components/StaticProfile/StaticProfile.test.js
@@ -1,36 +1,23 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import StaticProfile from './StaticProfile';
-// Test
-import { configure, shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import { createShallow } from '@material-ui/core/test-utils';
-// Material-UI
-import withStyles from '@material-ui/core/styles/withStyles';
-import MuiLink from '@material-ui/core/Link';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-// Icons
-import LocationOn from '@material-ui/icons/LocationOn';
-import LinkIcon from '@material-ui/icons/Link';
-import CalendarToday from '@material-ui/icons/CalendarToday';
+import { cleanup, render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import myStore from '../../store/myStore';
+import { MemoryRouter } from 'react-router-dom';
 
-configure({adapter: new Adapter()});
+const renderWithRedux = ({ children }) => render(
+	<Provider store={myStore}>
+		{children}
+	</Provider>
+);
 
-describe('<StaticProfile />', () => {
-	let shallow;
-	let wrapper;
+it('renders with Redux', () => {
+	const {} = renderWithRedux(
+		<MemoryRouter>
+			<StaticProfile />
+		</MemoryRouter>
+	);
+});
 
-	const myProps = {
-		profile: {},
-		classes: {}
-	}
-
-	beforeEach(() => {
-		shallow = createShallow();
-		wrapper = shallow(<StaticProfile {...myProps} />);
-	})
-
-	it('should render a Paper element', () => {
-		expect(wrapper.dive().find(Paper)).toHaveLength(1);
-	})
-})
+afterEach(cleanup);
