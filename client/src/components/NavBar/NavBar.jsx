0c422cfde29868e4612a24f3a54e772e56e58bc0
Ken Austria <kenaustriadev@gmail.com>
2021-05-25 16:20:08 -0700

    authenticated navbar

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,25 +1,58 @@
 import React from "react";
+// import CreatePost from "../CreatePost"
+// import Notifications from "../Notifications"
+import "./NavBar.css";
+// Redux
+import { useSelector } from "react-redux";
+// Libraries
+import { Link } from "react-router-dom";
+// Material-UI
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
-import { Link } from "react-router-dom";
-import "./NavBar.css";
+import Tooltip from "@material-ui/core/Tooltip";
+import IconButton from "@material-ui/core/IconButton";
+import HomeIcon from "@material-ui/icons/Home";
 
 const NavBar = () => {
+  const authenticated = useSelector((state) => state.users.authenticated);
   return (
-    <AppBar>
-      <Toolbar className='nav-container'>
-        <Button color='inherit' component={Link} to='/'>
-          GroupGrid
-        </Button>
-        <Button color='inherit' component={Link} to='/login'>
-          Login
-        </Button>
-        <Button color='inherit' component={Link} to='/signup'>
-          Signup
-        </Button>
-      </Toolbar>
-    </AppBar>
+    <div>
+      <Router>
+        <AppBar position='static'>
+          <Toolbar className='navContainer'>
+            {authenticated ? (
+              <>
+                {/* <CreatePost /> */}
+                <Link to='/'>
+                  <Tooltip title='Home' placement='top'>
+                    <IconButton>
+                      <HomeIcon className='iconColor' />
+                    </IconButton>
+                  </Tooltip>
+                </Link>
+                {/* <Notifications/> */}
+              </>
+            ) : (
+              <>
+                <Typography variant='h6'>
+                  <Button color='inherit' component={Link} to='/'>
+                    GroupGrid
+                  </Button>
+                </Typography>
+                <Button color='inherit' component={Link} to='/login'>
+                  Login
+                </Button>
+                <Button color='inherit' component={Link} to='/signup'>
+                  Signup
+                </Button>
+              </>
+            )}
+          </Toolbar>
+        </AppBar>
+      </Router>
+    </div>
   );
 };
 
